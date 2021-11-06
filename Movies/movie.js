067e5c1ac82c95ce9c14fb965bfef0e314ea009b
Kirk Garrison <Kirkgarrison24601@gmail.com>
2021-11-06 16:00:24 -0700

    added cache and cache time reset

--- Movies/movie.js
+++ Movies/movie.js
@@ -1,16 +1,34 @@
 const axios = require('axios');
+const cache = {}
 
 
-async function handleGetMovie(req, res) {
+function handleGetMovie(req, res) {
     const { query } = req.query;
+    if (cache[query] && (Date.now() - cache[query].timestamp) < 10000)  {
+        console.log('cache hit!' + query);
+        res.status(200).send(cache[query]);
+        return;
+    }
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_KEY}&query=${query}&language=en-US&page=1&include_adult=false`
-    const results = await axios.get(url);
-    const movieData = results.data.results.map(movies => new Movies(movies));
-    console.log(movieData);
-    res.status(200).send(movieData);
+
+    axios
+        .get(url)
+        .then(results => {
+            const movieData = results.data.results.map(movies => new Movies(movies));
+            cache[query] = movieData;
+            cache[query].timestamp = Date.now();
+            console.log('cache miss!' + cache[query].timestamp);
+            res.status(200).send(movieData);
+        
+        })
+        .catch(err => {
+            console.error('error', err);
+            res.status(500).send('error', err);
+        })
 }
+
 class Movies {
-    constructor (obj) {
+    constructor(obj) {
         this.title = obj.title;
         this.overview = obj.overview;
         this.image = `https://image.tmdb.org/t/p/w500${obj.poster_path}`;
--- Weather/weather.js
+++ Weather/weather.js
@@ -1,18 +1,35 @@
 const axios = require('axios');
+const cache = {}
+ function handleGetWeather(req, res) {
+    const { lat, lon } = req.query;
 
-async function handleGetWeather(req, res) {
-    const {lat, lon } = req.query;
-    const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHER_KEY}&include=minutely`;
-    const results = await axios.get(url);
-    const forecastData = results.data.data.map(forecast => new Forecast(forecast));
-    res.status(200).send(forecastData);
-}
-
-class Forecast {
-    constructor(obj) {
-        this.date = obj.datetime;
-        this.description = obj.weather.description;
+    if (cache[lat, lon] && (Date.now() - cache[lat, lon].timestamp) < 10000) {
+        console.log('cache hit!' + lat, lon);
+        res.status(200).send(cache[lat, lon]);
+        return;
     }
-}
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHER_KEY}&include=minutely`;
 
-module.exports = handleGetWeather;
+    axios
+        .get(url)
+        .then(results => {
+            const forecastData = results.data.data.map(forecast => new Forecast(forecast));
+            cache[lat, lon] = forecastData;
+            cache[lat, lon].timestamp = Date.now();
+            console.log('cache miss!' + cache[lat, lon].timestamp);
+            res.status(200).send(forecastData);
+        })
+        .catch(err => {
+            console.error('error', err);
+            res.status(500).send('error', err);
+        })
+ }
+
+    class Forecast {
+        constructor(obj) {
+            this.date = obj.datetime;
+            this.description = obj.weather.description;
+        }
+    }
+
+    module.exports = handleGetWeather;
